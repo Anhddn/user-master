b21101c3aeb8055955277c87773b9228f36cf03c
hoandd <duchoantk5@gmail.com>
2020-06-26 23:44:59 +0700

    addd

---
+++ EmployeeManager/Models/Entitys/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models.Entitys
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
