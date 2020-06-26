bd563d736062be6367d5a577d62ce3f23bfa0da2
Anh Ngoc <anh.dang-duong-ngoc@tek-experts.com>
2020-06-27 00:56:32 +0700

    Update validate

--- EmployeeManager/Models/Entitys/User.cs
+++ EmployeeManager/Models/Entitys/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,19 @@ namespace EmployeeManager.Models.Entitys
     {
         public int Id { get; set; }
 
+        [StringLength(250, ErrorMessage = "Max letters is 250")]
+        [MinLength(5, ErrorMessage = "Please add more letter")]
+        [Required(ErrorMessage = "This field is requied.")]
         public string UserName { get; set; }
 
         public string PassWord { get; set; }
 
+        [StringLength(250, ErrorMessage = "Max letters is 250")]
         public string DisplayName { get; set; }
 
         public DateTime BirthDay { get; set; }
 
+        [StringLength(50, ErrorMessage = "Max letters is 50")]
         public string Phone { get; set; }
 
         public string Address { get; set; }
